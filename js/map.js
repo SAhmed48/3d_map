f0f75ddae83646dab459831a38632640ff8ceb6e
Salman Ahmed <salman.esketchers@gmail.com>
2019-04-27 00:16:47 +0500

    Add loading gif

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@ class MapInitialize  {
 
     constructor() {
         Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NDNmYjI1My1iZjM5LTQ3OWMtYWQ2Ni0yYjQxZGVjMDY1ZTMiLCJpZCI6MTAyOTMsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NTYxMTA0NzV9.Ew4X_cVWMbARAMuJyCJhUQ0BmU8fuH9ggkloFVJnhGA';
+        this.loaded = false;
         this.viewer = this.loadViewer();
         this.loadTileSet();
     }
@@ -25,7 +26,23 @@ class MapInitialize  {
                 url: Cesium.IonResource.fromAssetId(21513)
             })
         );
-        this.viewer.zoomTo(tileset)
+        this.viewer.zoomTo(tileset);
+        setTimeout(this.checkLoad, 2000);
+    }
+
+    checkLoad() {
+        if (!this.loaded) {
+            var numTiles = this.viewer.scene.globe._surface._tileLoadQueue.length;
+            console.log(numTiles);
+            if (numTiles === 0) {
+                this.loaded = true;
+                // Hide the loading overlay.
+                document.getElementById("loading-overlay").style.display = "none";
+            }
+            else {
+                setTimeout(checkLoad, 100);
+            }
+        }
     }
 
 }
