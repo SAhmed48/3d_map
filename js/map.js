d31a1a078db68b465e303e0bb3229ed425cdc0ca
Salman Ahmed <salman.esketchers@gmail.com>
2019-04-28 23:57:09 +0500

    Change navigation settings.

--- js/map.js
+++ js/map.js
@@ -4,6 +4,8 @@ class MapInitialize  {
         Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NDNmYjI1My1iZjM5LTQ3OWMtYWQ2Ni0yYjQxZGVjMDY1ZTMiLCJpZCI6MTAyOTMsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NTYxMTA0NzV9.Ew4X_cVWMbARAMuJyCJhUQ0BmU8fuH9ggkloFVJnhGA';
         this.loaded = false;
         this.viewer = this.loadViewer();
+        this.viewer.extend(Cesium.viewerCesiumNavigationMixin, {});
+        this.cesiumNavigation = this.viewer.cesiumNavigation;
         this.loadTileSet();
     }
 
